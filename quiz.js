0fea2df87b3e8021b27cf83af2d9dc706a142d83
Nikhilan <125346233+itsnicky35@users.noreply.github.com>
2023-10-28 17:13:41 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,117 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'Who invented Computer?',
+    answers: [
+      { text: 'Charlers Babbage', correct: true },
+      { text: 'Thomas Edison', correct: false },
+      { text: 'Albert Einstein', correct: false },
+      { text: 'Issac Newton', correct: false },
+    ]
+  },
+  {
+    question: ' Which is the longest river on the earth?',
+    answers: [
+      { text: 'yangtze river', correct: false },
+      { text: 'Amazon river', correct: false },
+      { text: 'Nile', correct: true },
+      { text: 'Yellow river', correct: false },
+    ]
+  },
+  {
+    question: 'Largest island in the world is?',
+    answers: [
+      { text: 'New Guinea', correct: false },
+      { text: 'Greenland', correct: true },
+      { text: 'Madagascar', correct: false },
+      { text: 'Srilanka', correct: false },
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true },
+      { text: '9', correct: false },
+      { text: '12', correct: false },
+      
+    ]
+  }
+]
